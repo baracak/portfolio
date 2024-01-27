0e4ba83aeb85c01562a5b48d54575a021b3e7e74
Bara Kalinova <barunka.kalinova@gmail.com>
2024-01-27 17:13:05 +0100

    Insert images into hobbies page

--- app/hobbies/page.tsx
+++ app/hobbies/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import React from "react";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+
 export default function HobbiesPage() {
-  return <div>Hobbies</div>;
+  const list = [
+    {
+      title: "Crocheting",
+      img: "/images/crochetheart.png",
+    },
+    {
+      title: "Painting",
+      img: "/images/painting.png",
+    },
+    {
+      title: "Piano",
+      img: "/images/piano.png",
+    },
+    {
+      title: "Band",
+      img: "/images/band.png",
+    },
+  ];
+
+  return (
+    <div className="gap-6 grid grid-cols-1 sm:grid-cols-2">
+      {list.map((item, index) => (
+        <Card
+          shadow="sm"
+          key={index}
+          isPressable
+          onPress={() => console.log("item pressed")}
+        >
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              radius="none"
+              width="100%"
+              alt={item.title}
+              className="w-full object-cover h-[300px] rounded-t-large"
+              src={item.img}
+            />
+          </CardBody>
+          <CardFooter className="text-small justify-between">
+            <b>{item.title}</b>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -30,66 +30,68 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className="dark">
       <body>
         <Providers>
-          <Navbar
-            isBordered
-            isMenuOpen={isMenuOpen}
-            onMenuOpenChange={setIsMenuOpen}
-          >
-            <NavbarContent className="sm:hidden" justify="start">
-              <NavbarMenuToggle
-                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-              />
-            </NavbarContent>
+          <div className="flex flex-col items-center">
+            <Navbar
+              isBordered
+              isMenuOpen={isMenuOpen}
+              onMenuOpenChange={setIsMenuOpen}
+            >
+              <NavbarContent className="sm:hidden" justify="start">
+                <NavbarMenuToggle
+                  aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                />
+              </NavbarContent>
 
-            <NavbarContent className="sm:hidden pr-3" justify="center">
-              <NavbarBrand>
-                <AcmeLogo />
-                <p className="font-bold text-inherit">ACME</p>
-              </NavbarBrand>
-            </NavbarContent>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="start">
-              <NavbarBrand>
-                <Link href="/" color="foreground">
+              <NavbarContent className="sm:hidden pr-3" justify="center">
+                <NavbarBrand>
                   <AcmeLogo />
                   <p className="font-bold text-inherit">ACME</p>
-                </Link>
-              </NavbarBrand>
-            </NavbarContent>
+                </NavbarBrand>
+              </NavbarContent>
 
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-              {menuItems.map((item, index) => (
-                <NavbarItem key={`${item}-${index}`}>
-                  <Link
-                    href={item.pathname}
-                    color={
-                      pathname === item.pathname ? "primary" : "foreground"
-                    }
-                  >
-                    {item.name}
+              <NavbarContent className="hidden sm:flex gap-4" justify="start">
+                <NavbarBrand>
+                  <Link href="/" color="foreground">
+                    <AcmeLogo />
+                    <p className="font-bold text-inherit">ACME</p>
                   </Link>
-                </NavbarItem>
-              ))}
-            </NavbarContent>
+                </NavbarBrand>
+              </NavbarContent>
 
-            <NavbarMenu>
-              {menuItems.map((item, index) => (
-                <NavbarMenuItem key={`${item}-${index}`}>
-                  <Link
-                    className="w-full"
-                    color={
-                      pathname === item.pathname ? "primary" : "foreground"
-                    }
-                    href={item.pathname}
-                    size="lg"
-                  >
-                    {item.name}
-                  </Link>
-                </NavbarMenuItem>
-              ))}
-            </NavbarMenu>
-          </Navbar>
-          {children}
+              <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                {menuItems.map((item, index) => (
+                  <NavbarItem key={`${item}-${index}`}>
+                    <Link
+                      href={item.pathname}
+                      color={
+                        pathname === item.pathname ? "primary" : "foreground"
+                      }
+                    >
+                      {item.name}
+                    </Link>
+                  </NavbarItem>
+                ))}
+              </NavbarContent>
+
+              <NavbarMenu>
+                {menuItems.map((item, index) => (
+                  <NavbarMenuItem key={`${item}-${index}`}>
+                    <Link
+                      className="w-full"
+                      color={
+                        pathname === item.pathname ? "primary" : "foreground"
+                      }
+                      href={item.pathname}
+                      size="lg"
+                    >
+                      {item.name}
+                    </Link>
+                  </NavbarMenuItem>
+                ))}
+              </NavbarMenu>
+            </Navbar>
+            <div className="mx-auto max-w-[1280px] w-full p-6">{children}</div>
+          </div>
         </Providers>
       </body>
     </html>
